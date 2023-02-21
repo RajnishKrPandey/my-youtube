12aee40df504a74d3edd93ce0b8e2bf24074539f
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2023-02-22 00:32:28 +0530

    Create the reducers

---
+++ src/store/reducers/getHomePageVideos.ts
@@ -0,0 +1,19 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { YOUTUBE_API_URL } from "../../utils/constants";
+import { RootState } from "../index";
+
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+
+export const getHomePageVideos = createAsyncThunk(
+    "youtubeApp/homePageVideos",
+    async(isNext: boolean, {getState}) => {
+        const {
+            youtubeApp: {nextPageToken: nextPageTokenFromState, videos}, 
+        } = getState() as RootState;
+        const {
+            data: {items,nextPageToken}
+        } = await axios.get(`${YOUTUBE_API_URL}/videos?key=${API_KEY}/search?maxResults=20&q="reactjs projects"&key=${API_KEY}&part=snippet&type=video`);
+        console.log(items);
+    }
+    );

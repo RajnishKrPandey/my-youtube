0f2ddc35b957ce250817879b16d0b1d0f512cba0
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2023-02-22 01:02:10 +0530

    use useDispatch in Home and also make API call

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { getHomePageVideos } from '../store/reducers/getHomePageVideos';
 
 const Home = () => {
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
+
+  useEffect(() => {
+    dispatch(getHomePageVideos(false));
+  }, [dispatch]);
+
   return (
     <div className="max-h-screen overflow-hidden">
       <div style={{ height: '7.5vh' }}>
--- src/store/reducers/getHomePageVideos.ts
+++ src/store/reducers/getHomePageVideos.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { YOUTUBE_API_URL } from "../../utils/constants";
 import { RootState } from "../index";
 
-const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+const API_KEY = process.env.REACT_APP_YOUTUBE_DATA_API_KEY;
 
 export const getHomePageVideos = createAsyncThunk(
     "youtubeApp/homePageVideos",
@@ -12,8 +12,9 @@ export const getHomePageVideos = createAsyncThunk(
             youtubeApp: {nextPageToken: nextPageTokenFromState, videos}, 
         } = getState() as RootState;
         const {
-            data: {items,nextPageToken}
-        } = await axios.get(`${YOUTUBE_API_URL}/videos?key=${API_KEY}/search?maxResults=20&q="reactjs projects"&key=${API_KEY}&part=snippet&type=video`);
+            data: {items, nextPageToken}
+        } = await axios.get(
+            `${YOUTUBE_API_URL}/search?maxResults=20&q="reactjs projects"&key=${API_KEY}&part=snippet&type=video`);
         console.log(items);
     }
     );

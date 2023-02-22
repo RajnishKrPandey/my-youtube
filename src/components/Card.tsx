d90dc6c95f19d83a4105d7cec65a7f8dce4d7509
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2023-02-22 12:33:02 +0530

    Loads the thumbnail on the Home page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { HomePageVideos } from '../Types';
 
-const Card = ({data} : {data: HomePageVideos}) => {
-    const isData = data ? true : false;
-
+const Card = ({ data }: { data: HomePageVideos }) => {
   return (
+    <div className="w-64 h-60 flex gap-3 flex-col">
+      <div className="relative">
+        <span className="absolute bottom-3 right-3 text-sm bg-gray-900 px-2 py-0.5 z-10">
+          {data.videoDuration}
+        </span>
+        <Link to={`/watch/${data.videoId}`}>
+          <img
+            src={data.videoThumbnail}
+            className="h-44 w-72"
+            alt="thumbnail"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-    <div>Card</div>
-  )
-}
-
-export default Card
+export default Card;

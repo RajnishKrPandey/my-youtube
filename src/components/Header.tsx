636737a5418a57e7053a769b525a3a350f3e4a86
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2023-02-22 16:48:40 +0530

    Create a Reducer getSearchPageVideos, create changeSearchTerm, clearSearchTerm and implemented in Header C omponent

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,24 @@ import { TiMicrophone } from 'react-icons/ti';
 import { BsYoutube, BsBell } from 'react-icons/bs';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { RiVideoAddLine } from 'react-icons/ri';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { changeSearchTerm, clearSearchTerm, clearVideos } from '../store';
+import { getSearchPageVideos } from '../store/reducers/getSearchPageVideos';
 
 const Header = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm);
+  const handleSearch = () => {
+    if (location.pathname !== '/search') navigate('/search');
+    else {
+      dispatch(clearVideos());
+      dispatch(getSearchPageVideos(false));
+    }
+  };
+
   return (
     <div className="flex justify-between items-center px-9 h-14 bg-[#0F0F0F] opacity-95 sticky top-0 z-50">
       <div className="flex gap-8 items-center text-2xl">
@@ -21,7 +36,12 @@ const Header = () => {
         </Link>
       </div>
       <div className="flex items-center justify-center gap-5">
-        <form action="">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSearch();
+          }}
+        >
           <div className="flex bg-zinc-900 items-center h-10 px-4 pr-0 rounded-l-full rounded-r-full">
             <div className="flex gap-4 items-center pr-5">
               <div>
@@ -30,9 +50,16 @@ const Header = () => {
               <input
                 type="text"
                 className="w-96 bg-zinc-900 focus:outline-none border-none"
+                value={searchTerm}
+                onChange={(e) => dispatch(changeSearchTerm(e.target.value))}
               />
 
-              <AiOutlineClose className="text-3xl h-6 w-6 cursor-pointer" />
+              <AiOutlineClose
+                className={`text-3xl h-6 w-6 cursor-pointer ${
+                  !searchTerm ? 'invisible' : 'visible'
+                }`}
+                onClick={() => dispatch(clearSearchTerm())}
+              />
             </div>
             <button className="h-10 w-16 flex items-center justify-center bg-zinc-800 rounded-r-full">
               <AiOutlineSearch className="text-xl" />

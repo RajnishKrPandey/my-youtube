f938fe7c30635c93a8faeac3d8279046a1b903a6
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2023-02-22 17:22:03 +0530

    Create a Search Page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useNavigate } from 'react-router-dom';
+import Card from '../components/Card';
+import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
+import Spinner from '../components/Spinner';
+import { clearVideos } from '../store';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { getHomePageVideos } from '../store/reducers/getHomePageVideos';
+import { getSearchPageVideos } from '../store/reducers/getSearchPageVideos';
+import { HomePageVideos } from '../Types';
 
 const Search = () => {
-  return (
-    <div>Search</div>
-  )
-}
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
+  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm);
 
-export default Search
+  useEffect(() => {
+    dispatch(clearVideos());
+    if (searchTerm === '') navigate('/');
+    else {
+      dispatch(getSearchPageVideos(false));
+    }
+  }, [dispatch, navigate, searchTerm]);
+
+  return (
+    <div className="max-h-screen overflow-hidden">
+      <div style={{ height: '7.5vh' }}>
+        <Header />
+      </div>
+      <div className="flex" style={{ height: '92.5vh' }}>
+        <Sidebar />
+        {videos.length ? (
+          <InfiniteScroll
+            dataLength={videos.length}
+            next={() => dispatch(getSearchPageVideos(true))}
+            hasMore={videos.length < 500}
+            loader={<Spinner />}
+            height={700}
+          >
+            <div className="grid gap-y-14 gap-x-8 grid-cols-4 p-8">
+              {videos.map((item: HomePageVideos) => {
+                return <Card data={item} key={item.videoId} />;
+              })}
+            </div>
+          </InfiniteScroll>
+        ) : (
+          <Spinner />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
